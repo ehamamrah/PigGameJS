155f880adef9b93e6501690de759105880e6882b
Elayan Hamamrah <Elayan.hr@gmail.com>
2018-10-01 00:10:21 +0300

    Updating Scores && Change Active Player

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ activePlayer = 0;
 
 effectiveCells = ['current-0', 'current-1', 'score-0', 'score-1'];
 setEffectiveCellsToZero();
+hideDice();
 
 document.querySelector('.btn-roll').addEventListener('click', rollDice);
 
@@ -23,4 +24,24 @@ function rollDice() {
   diceDom               = document.querySelector('.dice');
   diceDom.style.display = 'block';
   diceDom.src           = 'images/dice-' + dice + '.png';
+
+  dice !== 1 ? addCurrentPlayerScore(dice) : changeCurrentPlayer();
+}
+
+function addCurrentPlayerScore(dice) {
+  roundScore += dice;
+  document.querySelector('#current-' + activePlayer).textContent = roundScore;
+}
+
+function changeCurrentPlayer() {
+  document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  roundScore   = 0;
+  document.querySelectorAll('#current-0, #current-1').textContent = 0;
+  document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+  hideDice();
+}
+
+function hideDice() {
+  document.querySelector('.dice').style.display = 'none';
 }
