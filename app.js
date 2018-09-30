1f9c72dc77a8f2879d3b623f5070ed10ed067879
Elayan Hamamrah <Elayan.hr@gmail.com>
2018-09-30 23:43:45 +0300

    Rolling Dice, Set Initial Values

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var scoresList, roundScore, activePlayer, dice;
+var scoresList, roundScore, activePlayer, effectiveCells;
 
 scoresList   = [0, 0];
 roundScore   = 0;
 activePlayer = 0;
-dice         = Math.floor(Math.random() * 6) + 1; // Generate number between 1 & 6
 
-document.querySelector('.dice').style.display                  = 'none'; // Hide dice img when load the page for first time
-document.querySelector('#current-' + activePlayer).textContent = dice;
+effectiveCells = ['current-0', 'current-1', 'score-0', 'score-1'];
+setEffectiveCellsToZero();
+
+document.querySelector('.btn-roll').addEventListener('click', rollDice);
+
+
+function setEffectiveCellsToZero() {
+  for(counter = 0; counter < effectiveCells.length; counter ++){
+    document.getElementById(effectiveCells[counter]).textContent = 0;
+  }
+}
+
+function rollDice() {
+  var dice, diceDom;
+
+  dice                  = Math.floor(Math.random() * 6) + 1; // Generate number between 1 & 6
+  diceDom               = document.querySelector('.dice');
+  diceDom.style.display = 'block';
+  diceDom.src           = 'images/dice-' + dice + '.png';
+}
