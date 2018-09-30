3f9a47117759cf68905958151b3f6ee745134b7f
Elayan Hamamrah <Elayan.hr@gmail.com>
2018-09-30 23:13:57 +0300

    Initial Values, Hide Dice

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-/*
-GAME RULES:
+var scoresList, roundScore, activePlayer, dice;
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+scoresList   = [0, 0];
+roundScore   = 0;
+activePlayer = 0;
+dice         = Math.floor(Math.random() * 6) + 1; // Generate number between 1 & 6
 
-*/
-
-var scoresList = [0, 0];
+document.querySelector('.dice').style.display                  = 'none'; // Hide dice img when load the page for first time
+document.querySelector('#current-' + activePlayer).textContent = dice;
